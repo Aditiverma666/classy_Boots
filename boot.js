7a5368a97c9d8c443f05e638fc3b4a94713c5089
Aditiverma666 <aditi121verma@gmail.com>
2023-07-01 22:59:31 +0530

    added favicon

--- boot.js
+++ boot.js
@@ -1,5 +1,10 @@
 const wrapper = document.querySelector(".sliderwrapper");
 const menuitems = document.querySelectorAll(".menuitem");
+
+
+
+
+
 menuitems.forEach((item,index) => {
     item.addEventListener("click",() => {
      wrapper.style.transform = `translateX(${-100 * index}vw)`; 
